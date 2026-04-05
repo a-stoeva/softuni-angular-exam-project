6fa8a7e1e34a88aadffe87ff6cd829213b021c4f
a-stoeva <stoevaaleksandriya@gmail.com>
2026-04-05 20:47:49 +0300

    add api service

--- client/src/app/tales/tales.component.ts
+++ client/src/app/tales/tales.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaleComponent } from '../tale/tale.component';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-tales',
@@ -8,6 +9,15 @@ import { TaleComponent } from '../tale/tale.component';
   templateUrl: './tales.component.html',
   styleUrl: './tales.component.css'
 })
-export class TalesComponent {
+export class TalesComponent implements OnInit{
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(): void {
+    this.apiService.getAll().subscribe(p => {
+      console.log(p);
+      
+    })
+  }
 
 }
