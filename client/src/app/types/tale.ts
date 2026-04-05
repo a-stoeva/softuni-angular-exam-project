729bc626a0da520f56278ac39d12a30ee3e2ba1e
a-stoeva <stoevaaleksandriya@gmail.com>
2026-04-05 23:59:58 +0300

    Add TravelTale interface

---
+++ client/src/app/types/tale.ts
@@ -0,0 +1,12 @@
+export interface TravelTale {
+    _id: string;
+    img: string;
+    title: string;
+    country: string;
+    city: string;
+    cost: string;
+    timeNeeded: string;
+    bestTimeToVisit: string;
+    myExperiance: string;
+    valuableTips: string;
+}
